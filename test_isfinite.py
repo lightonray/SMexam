f9471f9fda64f90a8ff435bc4a867a7b81e1f391
rei kasemi <reikasemi2001@gmail.com>
2023-01-04 11:22:15 +0200

    first test

---
+++ test_isfinite.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestDevisionResults(unittest.TestCase):
+    def test_finite_number(self):
+        self.assertEqual(my_finitefunction, FIN)
+
+
+if __name__ == '__main__':
+    unittest.main()
