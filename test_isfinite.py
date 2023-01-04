98f8829504a231c78f9def4b8a2ac2c2d2dcc194
rei kasemi <reikasemi2001@gmail.com>
2023-01-04 11:27:17 +0200

    test update

--- test_isfinite.py
+++ test_isfinite.py
@@ -4,7 +4,7 @@ from function_isfinite import my_finitefunction
 
 class TestDevisionResults(unittest.TestCase):
     def test_finite_number(self):
-        self.assertEqual(my_finitefunction(x), True)
+        self.assertEqual(my_finitefunction(5), True)
 
 
 if __name__ == '__main__':
