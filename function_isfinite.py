57622b8c3e6c1534a53648d9bd479db791d8939b
rei kasemi <reikasemi2001@gmail.com>
2023-01-04 12:37:54 +0200

    function after imprv

--- function_isfinite.py
+++ function_isfinite.py
@@ -1,7 +1,9 @@
+"import module"
 import math
 
 
 def my_finitefunction(x):
+    "function"
     if (type(x) == math.isnan(x) or type(x) == math.isinf(x)):
         return math.isfinite(x)
     if x == str:
