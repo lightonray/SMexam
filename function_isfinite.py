cf87865663d3be253bac948250aa5d935570fd93
rei kasemi <reikasemi2001@gmail.com>
2023-01-04 12:35:58 +0200

    code after improvement

--- function_isfinite.py
+++ function_isfinite.py
@@ -1,12 +1,11 @@
 import math
 
 
-
 def my_finitefunction(x):
-    if(x == float('nan') or type(x) == float('inf')):
+    if (type(x) == math.isnan(x) or type(x) == math.isinf(x)):
         return math.isfinite(x)
     if x == str:
-        raise TypeError("Input cannot be string")   
+        raise TypeError("Input cannot be string")
     if not x:
-        raise TypeError("Input cannot be empty")   
+        raise TypeError("Input cannot be empty")
     return math.isfinite(x)
--- test_isfinite.py
+++ test_isfinite.py
@@ -1,28 +1,32 @@
+"import"
 import unittest
 from function_isfinite import my_finitefunction
 
 
-
 class TestDevisionResults(unittest.TestCase):
+    "TEST INPUTS"
     def test_finite_number(self):
-        valid_input = 5;
+        "valid testing finite number"
+        valid_input = 5
         self.assertEqual(my_finitefunction(valid_input), True)
-        
+
     def test_incorrect_string_value(self):
+        "invalid testing string value"
         string_input = "hello"
         with self.assertRaises(TypeError):
-            self.assert_(my_finitefunction(string_input))
+            (my_finitefunction(string_input))
 
     def test_empty_value(self):
+        "invalid testing empty value"
         empty_input = None
         with self.assertRaises(TypeError):
-            self.assert_(my_finitefunction(empty_input))
-            
-    def test_NaN_value(self):
-        NaN_input = float('NaN')
-        self.assertEqual(my_finitefunction(NaN_input), False)
+            (my_finitefunction(empty_input))
 
-        
-    
-if __name__ == '__main__':
+    def test_nan_value(self):
+        "valid testing NaN value"
+        nan_input = float("NaN")
+        self.assertEqual(my_finitefunction(nan_input), False)
+
+
+if __name__ == "__main__":
     unittest.main()
