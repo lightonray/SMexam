8d1ce9b09818b2b9361726753acc5d4888b2c1c6
rei kasemi <reikasemi2001@gmail.com>
2023-01-04 12:43:20 +0200

    code final and coverage report

--- function_isfinite.py
+++ function_isfinite.py
@@ -4,10 +4,10 @@ import math
 
 def my_finitefunction(x):
     "function"
-    if (type(x) == math.isnan(x) or type(x) == math.isinf(x)):
-        return math.isfinite(x)
-    if x == str:
-        raise TypeError("Input cannot be string")
     if not x:
         raise TypeError("Input cannot be empty")
+    elif type(x) == str:
+        raise TypeError("Input cannot be string")
+    elif (type(x) == math.isnan(x) or type(x) == math.isinf(x)):
+        return math.isfinite(x)
     return math.isfinite(x)
